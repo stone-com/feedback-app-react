b9e89e0862f9a7ab7c39c3980a3a80160d0644dd
Stone Comstock <comstockstone@yahoo.com>
2022-11-28 14:17:24 -0800

    Delete button and Feedback stats component done

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackData from './data/feedbackData';
 import FeedbackList from './components/FeedbackList';
+import FeedbackStats from './components/FeedbackStats';
 
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData);
 
   const deleteFeedback = (id) => {
-    console.log('App', id);
+    // Alert/ask user to confirm before deleting the feedback
+    if (window.confirm('Are you sure you want to delete??')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
   };
   return (
     <>
       <Header />
       <div className='container'>
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
