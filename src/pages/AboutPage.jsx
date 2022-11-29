40b4671b396f7fbaf87b4ed327e0091c51ce03fb
Stone Comstock <comstockstone@yahoo.com>
2022-11-28 16:50:49 -0800

    Fixed/refactored a bit of the routing

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,5 @@
 import Card from '../components/shared/Card';
+import { Link } from 'react-router-dom';
 
 const AboutPage = () => {
   return (
@@ -9,7 +10,7 @@ const AboutPage = () => {
         <p>Version: 1.0.0</p>
       </div>
       <p>
-        <a href='/'>Back to Home!</a>
+        <Link to='/'>Back to Home!</Link>
       </p>
     </Card>
   );
